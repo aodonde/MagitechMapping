45ce96c604effc667f2bfc8a2abc43cb587d1870
Agoro Odonde <a.odonde@gmail.com>
2016-04-21 17:00:04 +0100

    The display of the graph with randomise data could not properly display. So added the ability to manual set the bounds of the x axis in order to alleviate this issue.
    The data conversion from a 2-D array to the DataPoint array format is successful however.

--- app/src/main/java/com/magitech/aodonde/magitechmapping/Main.java
+++ app/src/main/java/com/magitech/aodonde/magitechmapping/Main.java
@@ -11,12 +11,13 @@ import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.PointsGraphSeries;
 
+import java.sql.DataTruncation;
 import java.util.Random;
 
 
 public class Main extends AppCompatActivity {
     private static int MaxRandomNumber = 10;
-    private static int MinRandomNumber = 4;
+    private static int MinRandomNumber = 1;
 
     private PointsGraphSeries<DataPoint> RealTime1Series;
 
@@ -35,18 +36,19 @@ public class Main extends AppCompatActivity {
                 Log.d("Agoro", "Array X Data: " + Integer.toString(DataArray[i][0]));
             }
 
-        Log.d("Agoro-D", "Main Breakpoint 2 - testing out the graphview");
+        Log.d("Agoro-D", "Main Breakpoint 2 - testing out the graphview xml object");
 
         GraphView RealTime1 = (GraphView) findViewById(R.id.realtime1);
-        RealTime1Series = new PointsGraphSeries<>();
-        RealTime1.addSeries(RealTime1Series);
+        RealTime1.getViewport().setXAxisBoundsManual(true);
+        RealTime1.getViewport().setMinX(-10);
+        RealTime1.getViewport().setMaxX(10);
+        RealTime1.onDataChanged(false, false);
 
+        RealTime1Series = new PointsGraphSeries<DataPoint>(generatePoints(DataArray));
+        RealTime1.addSeries(RealTime1Series);
 
         Log.d("Agoro-D", "End of Main");
     }
-
-
-
 
 
 
@@ -76,6 +78,18 @@ public class Main extends AppCompatActivity {
             if(DataSort[i][0] > max) max = DataSort[i][0];
         }
         return max;
+    }
+
+    private DataPoint[] generatePoints( int[][] DataPointArray){
+        DataPoint[] values = new DataPoint[DataPointArray.length];
+            for(int i = 0; i<DataPointArray.length; i++){
+                int y = DataPointArray[i][1];
+                int x = DataPointArray[i][0];
+                DataPoint z = new DataPoint(x,y);
+                values[i] = z;
+            }
+        return values;
+
     }
 
 
