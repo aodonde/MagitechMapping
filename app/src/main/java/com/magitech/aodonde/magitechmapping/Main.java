4a5188433854f0d03e45e3881445599b500de795
Agoro Odonde <a.odonde@gmail.com>
2016-05-12 22:50:31 +0100

    added two new activities to the app

--- app/src/main/java/com/magitech/aodonde/magitechmapping/Main.java
+++ app/src/main/java/com/magitech/aodonde/magitechmapping/Main.java
@@ -3,6 +3,7 @@ package com.magitech.aodonde.magitechmapping;
  * Created by aodonde on 20/04/16.
  */
 
+import android.content.Intent;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -185,5 +186,13 @@ public class Main extends AppCompatActivity {
             Y[i][0] = y;
             ydatacounter++;
         }
+    }
+    public void AutoM(View view){
+        Intent intent = new Intent(this, Mapping.class);
+        startActivity(intent);
+    }
+    public void ManualM(View view){
+        Intent intent = new Intent(this, ManualMode.class);
+        startActivity(intent);
     }
 }
